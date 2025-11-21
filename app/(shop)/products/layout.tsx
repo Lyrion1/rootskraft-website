3425defcf65c79745d0593ae94559341ebef58e9
Lyrion1 <admin@renviait.co.uk>
2025-11-21 02:33:11 +0000

    Create ShopLayout component for product pages
    This layout component serves as a wrapper for dynamic product pages in the shop.

---
+++ app/(shop)/products/layout.tsx
@@ -0,0 +1,6 @@
+// app/(shop)/products/layout.tsx
+// This is a simple wrapper layout for the dynamic product pages.
+
+export default function ShopLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
